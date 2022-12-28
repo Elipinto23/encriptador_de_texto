987272339ccd71308956f67f0e94ade5d0fa988f
elipinto <elipinto23@gmail.com>
2022-12-28 14:05:31 -0500

    versiona inicial

---
+++ encriptador.js
@@ -0,0 +1,11 @@
+
+let texto1 = document.querySelector("#texto-para-encriptar").value;
+let texto2 = document.querySelector("#texto-para-desencriptar").value;
+const botonCifrar = document.querySelector(".boton-encriptar");
+console.log("texto1");
+
+
+
+function botonDesencriptar(){
+    alert('se presionó el párrafo');s
+}
