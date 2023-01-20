c02dac7773ae97e81e1275cf418a3eb1b3e390d0
elipinto <elipinto23@gmail.com>
2023-01-20 15:36:18 -0500

    funcionalidades de encriptado y desencritado y boton copiar

--- encriptador.js
+++ encriptador.js
@@ -1,11 +1,43 @@
+window.onload = (event) => {
+    
+   
+    
+    const botonDesencriptar = document.querySelector("#boton-encriptar");
+};
 
-let texto1 = document.querySelector("#texto-para-encriptar").value;
-let texto2 = document.querySelector("#texto-para-desencriptar").value;
-const botonCifrar = document.querySelector(".boton-encriptar");
-console.log("texto1");
 
+function encriptar(){
+    let texto1 = document.querySelector(".texto-para-encriptar").value.toLowerCase();
 
+    let textoEncriptado = texto1.replace(/e/img, "enter");
+        textoEncriptado = textoEncriptado.replace(/o/img, "ober");
+        textoEncriptado = textoEncriptado.replace(/i/img, "imes");
+        textoEncriptado = textoEncriptado.replace(/a/img, "ai");
+        textoEncriptado = textoEncriptado.replace(/u/img, "ufat");
 
-function botonDesencriptar(){
-    alert('se presionó el párrafo');s
+       document.querySelector("#texto-para-desencriptar").value = textoEncriptado;
+       
+       document.getElementById("mensaje").style.display="none";
+
+}
+
+function desencriptar(){
+
+    let texto1 = document.querySelector(".texto-para-encriptar").value.toLowerCase();
+
+    let textoEncriptado = texto1.replace (/entre/img,"e");
+        textoEncriptado = textoEncriptado.replace(/ober/img, "o");
+        textoEncriptado = textoEncriptado.replace(/imes/img, "i");
+        textoEncriptado = textoEncriptado.replace(/ai/img, "a");
+        textoEncriptado = textoEncriptado.replace(/ufat/img, "u");
+
+        document.querySelector("#texto-para-desencriptar").value = textoEncriptado; 
+
+}
+
+function copiar(){
+    
+    let texto = document.getElementById("texto-para-desencriptar");
+    texto.select();
+    document.execCommand("copy");
 }
